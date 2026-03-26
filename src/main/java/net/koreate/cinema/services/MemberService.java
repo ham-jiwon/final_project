e292cb4fe70b3360669f09bc353d11f0ff8e8c44
parkcheolwon <cheolwon18@gamil.com>
2026-03-26 16:14:24 +0900

    수정

--- src/main/java/net/koreate/cinema/services/MemberService.java
+++ src/main/java/net/koreate/cinema/services/MemberService.java
@@ -2,8 +2,15 @@ package net.koreate.cinema.services;
 
 import org.springframework.stereotype.Service;
 
+import net.koreate.cinema.vo.MemberVO;
+
 @Service
 public class MemberService {
+
+	public static void insertMember(MemberVO member) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 	
