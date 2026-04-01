d49df6375db8b938fd57d4541d8c87b428b028d5
test_git <parkofeden446@gmail.com>
2026-04-01 21:31:20 +0900

    미사용 import 제거

--- src/main/java/net/koreate/cinema/controller/CommentController.java
+++ src/main/java/net/koreate/cinema/controller/CommentController.java
@@ -1,10 +1,6 @@
 package net.koreate.cinema.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
