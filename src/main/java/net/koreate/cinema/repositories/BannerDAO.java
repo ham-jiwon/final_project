523332b7116c049cd10cf33a87622374031fdeb4
test_git <parkofeden446@gmail.com>
2026-04-13 23:10:22 +0900

    Banner DAO, VO, Service 생성

--- src/main/java/net/koreate/cinema/repositories/BannerDAO.java
+++ src/main/java/net/koreate/cinema/repositories/BannerDAO.java
@@ -34,6 +34,69 @@ public class BannerDAO {
 	    }
 	
 	    return list;
+	}
+	
+	public void insert(BannerVO vo) {
+	    String sql = "INSERT INTO banner(id, img_name, is_active) VALUES (banner_seq.NEXTVAL, ?, ?)";
+
+	    try(Connection conn = DBUtil.getConnection();
+	        PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+	        pstmt.setString(1, vo.getImgName());
+	        pstmt.setString(2, vo.getIsActive());
+
+	        pstmt.executeUpdate();
+
+	    } catch(Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	public void resetActive(){
+	    String sql = "UPDATE banner SET is_active = 'N'";
+
+	    try(Connection conn = DBUtil.getConnection();
+	        PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+	        pstmt.executeUpdate();
+
+	    } catch(Exception e){
+	        e.printStackTrace();
+	    }
 	}	
 	
-}
+	public void update(BannerVO vo) {
+	    String resetSql = "UPDATE banner SET is_active = 'N'";
+	    String updateSql = "UPDATE banner SET is_active = 'Y' WHERE id = ?";
+
+	    try(Connection conn = DBUtil.getConnection()) {
+	        // 1️) 전체 OFF
+	        PreparedStatement pstmt1 = conn.prepareStatement(resetSql);
+	        pstmt1.executeUpdate();
+	        
+	        // 2️) 선택한 것만 ON
+	        PreparedStatement pstmt2 = conn.prepareStatement(updateSql);
+	        pstmt2.setInt(1, vo.getId());
+	        pstmt2.executeUpdate();	        
+
+	    } catch(Exception e) {
+	        e.printStackTrace();
+	    }		
+	}
+	
+	public void delete(int id) {
+	    String sql = "DELETE FROM banner WHERE id = ?";
+
+	    try(Connection conn = DBUtil.getConnection();
+	        PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+	        pstmt.setInt(1, id);
+
+	        pstmt.executeUpdate();
+
+	    } catch(Exception e) {
+	        e.printStackTrace();
+	    }		
+	}	
+	
+} // end BannerDAO class
--- src/main/java/net/koreate/cinema/vo/BannerVO.java
+++ src/main/java/net/koreate/cinema/vo/BannerVO.java
@@ -4,7 +4,23 @@ import lombok.Data;
 
 @Data
 public class BannerVO {
-    private int id;
+    private Integer id;
     private String imgName;
     private String isActive;
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(String isActive) {
+        this.isActive = isActive;
+    }    
 }
