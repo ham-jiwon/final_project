2d27d43f6c6f3621dc1121fa74c266348355cdf6
PC <PC@192.168.219.104>
2026-04-22 23:45:00 +0900

    Merge branch 'develop' of https://github.com/ham-jiwon/final_project.git into develop

--- src/main/java/net/koreate/cinema/controller/BannerController.java
+++ src/main/java/net/koreate/cinema/controller/BannerController.java
@@ -75,11 +75,11 @@ public class BannerController {
             file.transferTo(dest1);
             
             // 2️) Git 경로 ⭐
-            String gitPath = "C:/Users/KTE/git/final_project/src/main/webapp/resources/img/banner/";
+            // String gitPath = "C:/Users/KTE/git/final_project/src/main/webapp/resources/img/banner/";
             
             // 다른 컴일 시
-//            String gitPath = new File("src/main/webapp/resources/img/banner/")
-//                    .getAbsolutePath() + "/";
+            String gitPath = new File("src/main/webapp/resources/img/banner/")
+                    .getAbsolutePath() + "/";
             
             File dir2 = new File(gitPath);
             if(!dir2.exists()){
